aa96882523bdff20c9c0f11ec207fef54c785fe4
BitCrusader <elikramergm@gmail.com>
2024-11-26 17:22:02 -0800

    .

--- ScriptHook/dllmain.cpp
+++ ScriptHook/dllmain.cpp
@@ -1,10 +1,26 @@
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 
+#include <GameDefines.cpp>
+
+static DWORD WINAPI init(LPVOID lpParam) {
+	Sleep(1000);
+	OutputDebugStringA("HOOK INIT\n");
+
+	// Find base address
+	auto baseAddress = (uintptr_t)(GetModuleHandle(NULL));
+
+	// Load GNames/GObjects
+	GNames = (TArray<FNameEntry*>*)(baseAddress + GNames_Offset);
+	GObjects = (TArray<UObject*>*)(baseAddress + GObjects_Offset);
+
+	return 0;
+}
+
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved) {
 	switch (ul_reason_for_call) {
 	case DLL_PROCESS_ATTACH:
-		OutputDebugStringA("HOOK INIT\n");
+		CreateThread(NULL, NULL, &init, NULL, NULL, NULL);
 		break;
 	case DLL_THREAD_ATTACH:
 	case DLL_THREAD_DETACH:
